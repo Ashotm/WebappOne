caab3b0ddc4a05ed0f0a7e7141263dea58070289
Ashotm <gmartirosyan@gmail.com>
2021-09-18 21:11:27 +0400

    Update application.py

--- application.py
+++ application.py
@@ -5,14 +5,23 @@ import pandas as pd
 import plotly.express as px
 import os
 import datetime as dt
-from git import Repo
+import requests
 
 ### Defining global variables ###
 
 ProductsDict = {'Mortgage': 1, 'Consumer loans': 2, 'Credit Card': 3, 'SME': 4, 'Student loans': 5, 'Auto loans': 6}
 #Dir = r'C:\Users\Admin\Desktop\WEBAPPONE\data/'
-Dir = 'https://api.github.com/Ashotm/WebappOne/tree/main/data/'
+#Dir = 'https://api.github.com/Ashotm/WebappOne/tree/main/data/'
 
+user = "Ashotm"
+repo = "WebappOne"
+
+url = "https://api.github.com/repos/Ashotm/WebappOne/git/trees/master?recursive=1".format(user, repo)
+r = requests.get(url)
+res = r.json()
+
+for file in res["tree"]:
+    st.write((file["path"])
 
 
 ### Making Streamlit app layout and setting up app controls ###
