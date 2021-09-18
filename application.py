1ec4a83a56ac13dc56f93a7284bff6a5302b789d
Ashotm <gmartirosyan@gmail.com>
2021-09-18 19:31:45 +0400

    Update application.py

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ import datetime as dt
 
 ProductsDict = {'Mortgage': 1, 'Consumer loans': 2, 'Credit Card': 3, 'SME': 4, 'Student loans': 5, 'Auto loans': 6}
 #Dir = r'C:\Users\Admin\Desktop\WEBAPPONE\data/'
-Dir = 'https://raw.githubusercontent.com/Ashotm/WebappOne/main/data/'
+Dir = 'https://github.com/Ashotm/WebappOne/tree/main/data'
 
 
 ### Making Streamlit app layout and setting up app controls ###
