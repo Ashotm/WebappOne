885f89c970f25f40f62e86a453e80e37badac126
Ashotm <gmartirosyan@gmail.com>
2021-09-18 23:24:27 +0400

    Update application.py

--- application.py
+++ application.py
@@ -5,18 +5,12 @@ import pandas as pd
 import plotly.express as px
 import os
 import datetime as dt
-import requests
+
 st.set_page_config(layout='wide')
 ### Defining global variables ###
 
 ProductsDict = {'Mortgage': 1, 'Consumer loans': 2, 'Credit Card': 3, 'SME': 4, 'Student loans': 5, 'Auto loans': 6}
 #Dir = r'C:\Users\Admin\Desktop\WEBAPPONE\data/'
-#Dir = 'https://api.github.com/Ashotm/WebappOne/tree/main/data/'
+Dir = 'https://github.com/Ashotm/webappone/blob/master/data'
 
-user = "Ashotm"
-repo = "WebappOne"
-
-url = "https://api.github.com/repos/Ashotm/WebappOne/git/trees/master?recursive=1".format(user, repo)
-r = requests.get(url)
-res = r.json()
-print(r)
+print(os.listdir(Dir))
