d795ab3566fc72ff886d7afc7ccf2bc4997beb35
Ashotm <gmartirosyan@gmail.com>
2021-09-18 20:22:17 +0400

    Update application.py

--- application.py
+++ application.py
@@ -5,12 +5,14 @@ import pandas as pd
 import plotly.express as px
 import os
 import datetime as dt
+from git import Repo
 
 ### Defining global variables ###
 
 ProductsDict = {'Mortgage': 1, 'Consumer loans': 2, 'Credit Card': 3, 'SME': 4, 'Student loans': 5, 'Auto loans': 6}
 #Dir = r'C:\Users\Admin\Desktop\WEBAPPONE\data/'
-#Dir = 'https://github.com/Ashotm/WebappOne/tree/main/data/'
+#Dir = 'https://api.github.com/Ashotm/WebappOne/tree/main/data/'
+
 
 
 ### Making Streamlit app layout and setting up app controls ###
@@ -18,4 +20,4 @@ ProductsDict = {'Mortgage': 1, 'Consumer loans': 2, 'Credit Card': 3, 'SME': 4, 
 st.set_page_config(layout='wide')
 st.title('WebApp One')
 #if i in [j.replace('.csv', '') for j in os.listdir(Dir)]:
-st.write('ppp')
+st.write(os.listdir(Dir))
