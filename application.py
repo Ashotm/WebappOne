c13ec9d5ef814c70e7ea8f7083996f26e8db6c6f
Ashotm <gmartirosyan@gmail.com>
2021-09-20 23:55:43 +0400

    Update application.py

--- application.py
+++ application.py
@@ -13,4 +13,10 @@ ProductsDict = {'Mortgage': 1, 'Consumer loans': 2, 'Credit Card': 3, 'SME': 4, 
 #Dir = r'C:\Users\Admin\Desktop\WEBAPPONE\data/'
 #Dir = 'https://github.com/Ashotm/webappone/blob/master/data'
 
-print(os.path.abspath(__file__))
+#print(os.path.abspath(__file__))
+print(os.listdir("./app/webappone/data"))
+
+
+
+
+
